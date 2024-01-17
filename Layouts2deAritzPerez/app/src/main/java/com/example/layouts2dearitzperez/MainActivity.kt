5e4e4623db761327c1de8f44123b84cf2a148f3d
dedalo6 <aperezdmor@educacion.navarra.es>
2024-01-18 00:26:02 +0100

    Terminado Layouts 2

---
+++ Layouts2deAritzPerez/app/src/main/java/com/example/layouts2dearitzperez/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.layouts2dearitzperez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraint_layout_buttons_and_chains)
+    }
+}
